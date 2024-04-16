d7d377a9b10ec284ca9f6e9e29694237d3bfee35
301kevin <166409233+301kevin@users.noreply.github.com>
2024-04-16 21:06:09 +0900

    Add files via upload

---
+++ 07-Casting/src/Exam1.java
@@ -0,0 +1,24 @@
+
+public class Exam1 {
+	public static void main(String[] args) {
+		short s = 100;
+		int i = s;
+		long l = i;
+		float f = l;
+		double d = f;
+		
+		System.out.println("s = " + s);
+		System.out.println("i = " + i);
+		System.out.println("l = " + l);
+		System.out.println("f = " + f);
+		System.out.println("d = " + d);
+		System.out.println("---------------");
+		
+		char ch = 'a';
+		int num = ch;
+		System.out.println("ch = " + ch);
+		System.out.println("num = " +num);
+		System.out.println((int)'가');
+	}
+
+}
